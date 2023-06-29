c25c6ccab944f4e396a7a1be69067e68219c56f5
Aksultan Aitkozhin <52781023+AksultanAitkozha@users.noreply.github.com>
2023-06-30 03:12:51 +0600

    added questions

--- main.py
+++ main.py
@@ -36,31 +36,51 @@ async def get_questions(category: str):
     questions_data = {
         "timeline": [
             {
-                "question": "кто ты, юный падаван: iOS или fullstack",
+                "question": "Кто ты, юный падаван: iOS или fullstack",
                 "image": "https://drive.google.com/file/d/1Jf6zcJUKKuum7_FBRctslKYOlw3H0Am5/view?usp=drive_link",
                 "answer": "yes"
             },
             {
-                "question": "твой любимый ментор?",
+                "question": "Твой любимый ментор?",
                 "image": "https://drive.google.com/file/d/1Jf6zcJUKKuum7_FBRctslKYOlw3H0Am5/view?usp=drive_link",
                 "answer": "optional"
             },
+            {
+                "question": "Запитчил идею Арману?",
+                "image": "https://drive.google.com/file/d/1cEErufIAhYnr7bx0Mof_qmmCRfZg1AYe/view?usp=drive_link",
+                "answer": "yes"
+            },
+            {
+                "question": "Запитчил идею Арману?",
+                "image": "https://drive.google.com/file/d/1cEErufIAhYnr7bx0Mof_qmmCRfZg1AYe/view?usp=drive_link",
+                "answer": "yes"
+            },
+            {
+                "question": "шаришь за лангчейн? а за векторные базы данных? 🤔",
+                "image": "https://drive.google.com/file/d/15uy5T45Jkb5aEdodNeCUHIwzu4qgv8ZD/view?usp=drive_link",
+                "answer": "yes"
+            }
         ],
         "random": [
             {
-                "question": "Спал на лекции? 🤔",
+                "question": "Спал на лекции?",
                 "image": "https://drive.google.com/file/d/1h_hg0D8vxNd23iWSqXayIp2OaGPOfXAo/view?usp=drive_link",
                 "answer": "noR"
             },
             {
-                "question": "вжжж... на вас напал клоп!",
+                "question": "Вжжж... на вас напал клоп!",
                 "image": "https://drive.google.com/file/d/1u5jDh-iy_uhyJaaUwAgu-kdPqgcBWlk3/view?usp=drive_link",
                 "answer": "yesR"
             },
             {
-                "question": "кто из них Оссейн?",
+                "question": "Кто из них Оссейн?",
                 "image": "https://drive.google.com/file/d/1zQyI0yZFKhKivmKd8NS_u9xU5IGd50KF/view?usp=drive_link",
                 "answer": "yesR"
+            },
+            {
+                "question": "Кто разрабатывает ImamAI?",
+                "image": "https://drive.google.com/file/d/12jZkIsg0mSpZCHDOxH29ksit7ljGfK5i/view?usp=drive_link",
+                "answer": "ArthurR"
             }
         ]
     }
